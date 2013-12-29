dc4e2211ce5056b8a977c3da185a2326a8e1a436
kudin <kudin@kudin-Lenovo-G780>
2013-12-29 13:39:45 +0300

    Мелкие правки

--- install/index.php
+++ install/index.php
@@ -1,10 +1,29 @@
 <?
 class conf extends CModule {
-    public $MODULE_ID           = __CLASS__;
-    public $MODULE_VERSION      = '1.1';
-    public $MODULE_VERSION_DATE = '2013.12.27';
-    public $MODULE_NAME         = 'Настройки сайта';
-    public $MODULE_DESCRIPTION  = 'Модуль для редактирвоания собственных настроек через админку';
+    
+    var $MODULE_ID = 'conf';
+    var $MODULE_VERSION;
+    var $MODULE_VERSION_DATE;
+    var $MODULE_NAME;
+    var $MODULE_DESCRIPTION;
+    var $MODULE_GROUP_RIGHTS;
+    var $errors = array();
+ 
+    function __construct() {
+        $arModuleVersion = array();
+
+        $path = str_replace("\\", "/", __FILE__);
+        $path = substr($path, 0, strlen($path) - strlen("/index.php"));
+        include($path . "/version.php");
+
+        if (is_array($arModuleVersion) && array_key_exists("VERSION", $arModuleVersion)) {
+            $this->MODULE_VERSION = $arModuleVersion["VERSION"];
+            $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
+        }
+
+        $this->MODULE_NAME = 'Настройки сайта';
+        $this->MODULE_DESCRIPTION = '';
+    }
 
     public function DoInstall() {
         RegisterModule($this->MODULE_ID);
@@ -13,4 +32,5 @@ class conf extends CModule {
     public function DoUninstall(){
         UnRegisterModule($this->MODULE_ID);
     }
+    
 }
---
+++ install/version.php
@@ -0,0 +1,5 @@
+<?php
+$arModuleVersion = array(
+	"VERSION" => "1.1",
+	"VERSION_DATE" => "2013-12-29 13:25:00"
+); 
